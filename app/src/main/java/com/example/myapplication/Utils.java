72fdd47c72d20b12325cfa15e29b8dd2e33d890d
jagereren <75220653+jagereren@users.noreply.github.com>
2022-05-19 22:19:31 +0200

    Mail tentative

---
+++ app/src/main/java/com/example/myapplication/Utils.java
@@ -0,0 +1,10 @@
+package com.example.myapplication;
+
+public class Utils {
+
+    //This is your from email
+    public static final String EMAIL = "souam.yacine@gmail.com";
+
+    //This is your from email password
+    public static final String PASSWORD = "yacinPRO2ErenJager4";
+}
